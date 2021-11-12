978666188e228f802004f4559e40485f9f8405b8
usuario <usuario@pc20-059>
2021-11-12 09:58:59 +0100

    Primera version

---
+++ src/cuenta/Cuenta.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuenta;
+
+/**
+ *
+ * @author usuario
+ */
+public class Cuenta {
+    
+    private int id;
+    private int balance;
+    
+    public Cuenta(int id,int balance){
+//        this.balance= balance;
+//        this.id = id;
+                
+    }
+    public void retirar(int id){
+        
+    }
+    public void depositar(int id){
+    
+    }
+    
+}
